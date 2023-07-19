3074cea2153ad1764af3ac6f1e016fa4be3101f9
Agustin <agcadu@gmail.com>
2023-07-19 13:16:28 +0200

    modifico firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzlJTEnZKLr5Qrkxz_-1HntwnQgyx72jQ",
-  authDomain: "llauhome-a326e.firebaseapp.com",
-  projectId: "llauhome-a326e",
-  storageBucket: "llauhome-a326e.appspot.com",
-  messagingSenderId: "635066839344",
-  appId: "1:635066839344:web:a4e77ba3ff839e7ced18f5"
+  
 };
 
 export const initFirebase = initializeApp(firebaseConfig);
